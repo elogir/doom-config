1419444ca95e8c5f0c4f9bd8e76b2169c575b706
raphael.nahoum <raphael.nahoum@epita.fr>
2025-01-19 12:20:36 +0100

    last old one

--- config.el
+++ config.el
@@ -94,11 +94,11 @@
               (([remap complete-symbol] . #'company-complete)
                ("M-`" . #'company-complete))))
 :custom
-(company-idle-delay nil)
-(company-frontends '(company-pseudo-tooltip-frontend))
-(company-tooltip-align-annotations t)
-(company-tooltip-limit 6)
-(company-tooltip-minimum 6))
+;; (company-idle-delay nil)
+;; (company-frontends '(company-pseudo-tooltip-frontend))
+;; (company-tooltip-align-annotations t)
+;; (company-tooltip-limit 6)
+;; (company-tooltip-minimum 6))
 
 (use-package! lsp-java
   :init
@@ -116,22 +116,7 @@
   (treemacs-collapse-dirs 4)
   (treemacs-indentation 1))
 
-;; (use-package! gptel
-;;   :custom
-;;   (gptel-api-key "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")
-;;   (gptel-default-mode 'org-mode)
-;;   :config
-;;   (add-hook 'gptel-post-stream-hook 'gptel-auto-scroll)
-;;   (add-hook 'gptel-post-response-functions 'gptel-end-of-response))
-
-;; (add-to-list 'load-path "/home/rigole/Documents/Git-Repos/gptel-extensions.el")
-;; (require 'gptel-extensions)
-
 (use-package! v-mode
-  :config
-  :bind
-  (:map v-mode-map
-        ("M-z" . v-menu))
   :mode ("\\(\\.v?v\\|\\.vsh\\)$" . 'v-mode))
 
 (with-eval-after-load 'lsp-mode
@@ -143,71 +128,71 @@
                     :activation-fn (lsp-activate-on "v")
                     :server-id 'v-analyzer)))
 
-(use-package! realgud)
-(use-package! realgud-lldb)
+;; (use-package! realgud)
+;; (use-package! realgud-lldb)
 
-(add-to-list 'load-path "~/.config/emacs/site-lisp")
+;; (add-to-list 'load-path "~/.config/emacs/site-lisp")
 ;; (load-library "doxygen-highlight.el")
-(load-library "gendoxy.el")
-(autoload 'tiger-mode "tiger" "Load tiger-mode" t)
-(add-to-list 'auto-mode-alist '("\.ti[gh]$" . tiger-mode))
+;; (load-library "gendoxy.el")
+;; (autoload 'tiger-mode "tiger" "Load tiger-mode" t)
+;; (add-to-list 'auto-mode-alist '("\.ti[gh]$" . tiger-mode))
 
-(defface doxygen-verbatim-face
-  '((default :inherit default))
-  "Face used to show Doxygen block regions"
-  :group 'font-lock-faces)
+;; (defface doxygen-verbatim-face
+;;   '((default :inherit default))
+;;   "Face used to show Doxygen block regions"
+;;   :group 'font-lock-faces)
 
-(defface doxygen-match-face
-  '((default :inherit default)
-    (t :underline t))
-  "Face used to show Doxygen region start end commands"
-  :group 'font-lock-faces)
+;; (defface doxygen-match-face
+;;   '((default :inherit default)
+;;     (t :underline t))
+;;   "Face used to show Doxygen region start end commands"
+;;   :group 'font-lock-faces)
 
-(defconst custom-font-lock-doc-comments
-  `(
-    (,(concat
-       "\\(?:"
-       "[\\@][a-z]+"
-       "\\|"
-       "[\\@]\\(?:\\\\\\|@\\|&\\|#\\|<\\|>\\|%\\|\"\\|\\.\\|::\\||\\|---?\\|~[a-z]*\\)"
-       "\\)")
-     0 ,c-doc-markup-face-name prepend nil)
-    (,(concat
-       "\\(?:"
-       "[A-Za-z_0-9]+(\\([A-Za-z_0-9:&*, ]*)\\)?"
-       "\\|"
-       "\\(?:[A-Za-z_0-9]+\\|\\s-\\)\\(?:::\\|#\\)~?[A-Za-z_0-9]+(?\\(?:[A-Za-z_0-9:&*, \t]*)\\)?"
-       "\\|"
-       "[A-Za-z_0-9/]+\\.\\(?:cpp\\|cxx\\|cc\\|c\\|hpp\\|hxx\\|hh\\|h\\)"
-       "\\)")
-     0 font-lock-function-name-face prepend nil)
-    ("https?://[^[:space:][:cntrl:]]+"
-     0 font-lock-keyword-face prepend nil)
-    (,(concat "</?\\sw"
-              "\\("
-              (concat "\\sw\\|\\s \\|[=\n\r*.:]\\|"
-                      "\"[^\"]*\"\\|'[^']*'")
-              "\\)*>")
-     0 ,c-doc-markup-face-name prepend nil)
-    ("[A-Za-z0-9.]+@[A-Za-z0-9_]+\\.[A-Za-z0-9_.]+"
-     0 font-lock-keyword-face prepend nil)
-    ("\"[^\"[:cntrl:]]+\""
-     0 ,c-doc-face-name prepend nil)
+;; (defconst custom-font-lock-doc-comments
+;;   `(
+;;     (,(concat
+;;        "\\(?:"
+;;        "[\\@][a-z]+"
+;;        "\\|"
+;;        "[\\@]\\(?:\\\\\\|@\\|&\\|#\\|<\\|>\\|%\\|\"\\|\\.\\|::\\||\\|---?\\|~[a-z]*\\)"
+;;        "\\)")
+;;      0 ,c-doc-markup-face-name prepend nil)
+;;     (,(concat
+;;        "\\(?:"
+;;        "[A-Za-z_0-9]+(\\([A-Za-z_0-9:&*, ]*)\\)?"
+;;        "\\|"
+;;        "\\(?:[A-Za-z_0-9]+\\|\\s-\\)\\(?:::\\|#\\)~?[A-Za-z_0-9]+(?\\(?:[A-Za-z_0-9:&*, \t]*)\\)?"
+;;        "\\|"
+;;        "[A-Za-z_0-9/]+\\.\\(?:cpp\\|cxx\\|cc\\|c\\|hpp\\|hxx\\|hh\\|h\\)"
+;;        "\\)")
+;;      0 font-lock-function-name-face prepend nil)
+;;     ("https?://[^[:space:][:cntrl:]]+"
+;;      0 font-lock-keyword-face prepend nil)
+;;     (,(concat "</?\\sw"
+;;               "\\("
+;;               (concat "\\sw\\|\\s \\|[=\n\r*.:]\\|"
+;;                       "\"[^\"]*\"\\|'[^']*'")
+;;               "\\)*>")
+;;      0 ,c-doc-markup-face-name prepend nil)
+;;     ("[A-Za-z0-9.]+@[A-Za-z0-9_]+\\.[A-Za-z0-9_.]+"
+;;      0 font-lock-keyword-face prepend nil)
+;;     ("\"[^\"[:cntrl:]]+\""
+;;      0 ,c-doc-face-name prepend nil)
 
-    ("[^\\@]\\([\\@]f.+?[\\@]f\\$\\)"
-     1 'doxygen-verbatim-face prepend nil)
-    (,(concat
-       "[^\\@]"
-       "\\([\\@]\\(?:verbatim\\|endverbatim\\|code\\|endcode\\|f{\\|f\\[\\|f}\\|f]\\)\\)")
-     1 'doxygen-match-face prepend nil)
-    ))
+;;     ("[^\\@]\\([\\@]f.+?[\\@]f\\$\\)"
+;;      1 'doxygen-verbatim-face prepend nil)
+;;     (,(concat
+;;        "[^\\@]"
+;;        "\\([\\@]\\(?:verbatim\\|endverbatim\\|code\\|endcode\\|f{\\|f\\[\\|f}\\|f]\\)\\)")
+;;      1 'doxygen-match-face prepend nil)
+;;     ))
 
-(defconst custom-font-lock-keywords
-  `((,(lambda (limit)
-        (c-font-lock-doc-comments "/\\(//\\|\\*[\\*!][^\\*!]\\)"
-            limit custom-font-lock-doc-comments)))))
+;; (defconst custom-font-lock-keywords
+;;   `((,(lambda (limit)
+;;         (c-font-lock-doc-comments "/\\(//\\|\\*[\\*!][^\\*!]\\)"
+;;             limit custom-font-lock-doc-comments)))))
 
-(setq-default c-doc-comment-style (quote (custom)))
+;; (setq-default c-doc-comment-style (quote (custom)))
 
 ;; (setq apheleia-mode-alist '(("\\.ll\\'" . nil) ("\\.yy\\'" . nil)))
 
@@ -240,16 +225,17 @@
       orig-result)))
 (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
-(use-package! devdocs
-  :hook ((c++-ts-mode . (lambda () (setq-local devdocs-current-docs '("cpp")))))
-  :bind (:map global-map
-              (("C-h h" . #'devdocs-lookup))))
+;; (use-package! devdocs
+;;   :hook ((c++-ts-mode . (lambda () (setq-local devdocs-current-docs '("cpp")))))
+;;   :bind (:map global-map
+;;               (("C-h h" . #'devdocs-lookup))))
 
 (use-package! lsp-mode
   :hook (
          (dockerfile-mode . lsp)
          (c-ts-mode . lsp)
          (c++-ts-mode . lsp)
+         (v-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :bind (:map lsp-mode-map
               ("M-RET" . #'lsp-execute-code-action))
@@ -259,101 +245,109 @@
 
 (use-package! projectile)
 
-(use-package! org-latex-preview
-  :config
-  (plist-put org-latex-preview-appearance-options
-             :page-width 0.8)
-  (setq org-latex-preview-process-default 'dvisvgm)
-  (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
-  (add-hook 'org-latex-preview-auto-ignored-commands 'next-line)
-  (add-hook 'org-latex-preview-auto-ignored-commands 'previous-line)
-  (setq org-latex-preview-numbered t)
-  (setq org-latex-preview-live t)
-  (setq org-latex-preview-live-debounce 0.25))
+;; (use-package! org-latex-preview
+;;   :config
+;;   (plist-put org-latex-preview-appearance-options
+;;              :page-width 0.8)
+;;   (setq org-latex-preview-process-default 'dvisvgm)
+;;   (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
+;;   (add-hook 'org-latex-preview-auto-ignored-commands 'next-line)
+;;   (add-hook 'org-latex-preview-auto-ignored-commands 'previous-line)
+;;   (setq org-latex-preview-numbered t)
+;;   (setq org-latex-preview-live t)
+;;   (setq org-latex-preview-live-debounce 0.25))
 
-(use-package! aas
-  :hook (org-mode . aas-activate-for-major-mode)
-  :config
-  (aas-set-snippets 'org-mode
-                    :cond #'texmathp
-                    "/ens" '(yas "\\mathbb{$1} ")
-                    "/fr" '(yas "\\frac{$1}{$2} ")
-                    "_" '(yas "_{$1}")
-                    "^" '(yas "^{$1}")
-                    "dans" "\\in"
-                    "Sigma" "\\Sigma"
-                    "/pt" "\\forall "
-                    "/ex" "\\exists "
-                    "RR" "\\mathbb{R} "
-                    "NN" "\\mathbb{N} "
-                    "PP" "\\mathbb{P} "
-                    "<=" "\\leq"
-                    ">=" "\\geq"
-                    "=>" "\\implies"
-                    "->" "\\mapsto"
-                    "iif" "\\Longleftrightarrow"
-                    "/abs" '(yas "\\displaystyle\\left\\lvert $1 \\right\\rvert $0")
-                    "/norme" '(yas "\\lVert $1 \\rVert")
-                    "/inf" "\\infty"
-                    "/lim" '(yas "\\underset{$1 \\to $2}{lim} $3 = $0")
-                    "/dp" '(yas "\\frac{\\partial{$1}}{\\partial{$2}}")
-                    "/dd" '(yas "\\frac{d$1}{d$2}")
-                    "/t1" '(yas "\\text{$1} ")
-                    "/t2" '(yas "\\quad\\text{$1}\\quad ")
-                    "/t3" '(yas "\\qquad\\text{$1}\\qquad ")
-                    "/pp" '(yas "\\left( $1 \\right)")
-                    "~" "\\sim"
-                    "/V" '(yas "\\sqrt{$1}")
-                    "/int" '(yas "\\int_{$1}^{$2}$3d$4"))
-  (aas-set-snippets 'org-mode
-                    ";i" '(yas "\\\\( $1 \\\\) $0")
-                    ";e" '(yas "\\\\[ $1 \\\\] $0")))
+;; (use-package! aas
+;;   :hook (org-mode . aas-activate-for-major-mode)
+;;   :config
+;;   (aas-set-snippets 'org-mode
+;;     :cond #'texmathp
+;;     "/ens" '(yas "\\mathbb{$1} ")
+;;     "/fr" '(yas "\\frac{$1}{$2} ")
+;;     "_" '(yas "_{$1}")
+;;     "^" '(yas "^{$1}")
+;;     "dans" "\\in"
+;;     "Sigma" "\\Sigma"
+;;     "/pt" "\\forall "
+;;     "/ex" "\\exists "
+;;     "RR" "\\mathbb{R} "
+;;     "NN" "\\mathbb{N} "
+;;     "PP" "\\mathbb{P} "
+;;     "<=" "\\leq"
+;;     ">=" "\\geq"
+;;     "!=" "\\neq"
+;;     "=>" "\\implies"
+;;     "->" "\\mapsto"
+;;     "-->" "\\longrightarrow"
+;;     "iif" "\\Longleftrightarrow"
+;;     "/abs" '(yas "\\displaystyle\\left\\lvert $1 \\right\\rvert $0")
+;;     "/norme" '(yas "\\lVert $1 \\rVert")
+;;     "/bmat" '(yas "\\begin{bmatrix} $1 \\end{bmatrix}")
+;;     "/inf" "\\infty"
+;;     "/lim" '(yas "\\underset{$1 \\to $2}{lim} $3 = $0")
+;;     "/dp" '(yas "\\frac{\\partial{$1}}{\\partial{$2}}")
+;;     "/dd" '(yas "\\frac{d$1}{d$2}")
+;;     "/t1" '(yas "\\text{$1} ")
+;;     "/t2" '(yas "\\quad\\text{$1}\\quad ")
+;;     "/t3" '(yas "\\qquad\\text{$1}\\qquad ")
+;;     "/pp" '(yas "\\left( $1 \\right)")
+;;     "~" "\\sim"
+;;     "/V" '(yas "\\sqrt{$1}")
+;;     "/int" '(yas "\\int_{$1}^{$2}$3d$4"))
+;;   (aas-set-snippets 'org-mode
+;;     ";i" '(yas "\\\\( $1 \\\\) $0")
+;;     ";e" '(yas "\\\\[ $1 \\\\] $0")))
 
-(use-package! yasnippet
-  :hook ((lsp-mode . yas-minor-mode) (org-mode . yas-minor-mode))
-  :bind (:map yas-keymap
-	      ("C-c SPC" . #'yas-expand)
-              ("TAB" . #'yas-next-field))
-  :config
-  (yas-reload-all))
+;; (use-package! yasnippet
+;;   :hook ((lsp-mode . yas-minor-mode) (org-mode . yas-minor-mode))
+;;   :bind (:map yas-keymap
+;; 	      ("C-c SPC" . #'yas-expand)
+;;               ("TAB" . #'yas-next-field))
+;;   :config
+;;   (yas-reload-all))
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '((python . t)))
 
-(use-package! org
-  :bind (:map org-mode-map
-              ("C-c C-c" . #'org-babel-execute-src-block)
-              ("<tab>" . nil)))
+;; (use-package! org
+;;   :bind (:map org-mode-map
+;;               ("C-c C-c" . #'org-babel-execute-src-block)
+;;               ("<tab>" . nil)))
 
-(use-package toc-org
-  :hook (org-mode . toc-org-mode))
+;; (use-package toc-org
+;;   :hook (org-mode . toc-org-mode))
 
-(with-eval-after-load 'ox-latex
-  (add-to-list 'org-latex-classes
-               '("org-plain-latex"
-                 "\\documentclass{article}
-           [NO-DEFAULT-PACKAGES]
-           \\usepackage{amsmath}
-           \\usepackage{graphicx}
-           \\usepackage{amssymb}
-           \\usepackage{hyperref}
-           [EXTRA]"
-                 ("\\section{%s}" . "\\section*{%s}")
-                 ("\\subsection{%s}" . "\\subsection*{%s}")
-                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+;; (with-eval-after-load 'ox-latex
+;;   (add-to-list 'org-latex-classes
+;;                '("org-plain-latex"
+;;                  "\\documentclass{article}
+;;            [NO-DEFAULT-PACKAGES]
+;;            \\usepackage{amsmath}
+;;            \\usepackage{graphicx}
+;;            \\usepackage{amssymb}
+;;            \\usepackage{hyperref}
+;;            \\usepackage{textalpha}
+;;            [EXTRA]"
+;;                  ("\\section{%s}" . "\\section*{%s}")
+;;                  ("\\subsection{%s}" . "\\subsection*{%s}")
+;;                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;;                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+;;                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-(use-package! engrave-faces-latex
-  :after ox-latex)
+;; (use-package! engrave-faces-latex
+;;   :after ox-latex)
 
-(setq org-latex-listings 'engraved
-      org-latex-engraved-theme 'doom-one-light)
+;; (setq org-latex-listings 'engraved
+;;       org-latex-engraved-theme 'doom-one-light)
 
-(org-export-update-features 'latex
-  (no-protrusion-in-code
-   :condition t
-   :when (microtype engraved-code)
-   :snippet "\\ifcsname Code\\endcsname\n  \\let\\oldcode\\Code\\renewcommand{\\Code}{\\microtypesetup{protrusion=false}\\oldcode}\n\\fi"
-   :after (engraved-code microtype)))
+;; (org-export-update-features 'latex
+;;   (no-protrusion-in-code
+;;    :condition t
+;;    :when (microtype engraved-code)
+;;    :snippet "\\ifcsname Code\\endcsname\n  \\let\\oldcode\\Code\\renewcommand{\\Code}{\\microtypesetup{protrusion=false}\\oldcode}\n\\fi"
+;;    :after (engraved-code microtype)))
+
+;; (use-package! nov
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
--- packages.el
+++ packages.el
@@ -9,40 +9,44 @@
                                         ;  :recipe
                                         ;  (:host github :repo "elogir/dap-mode"))
                                         ;(package! gptel)
-(package! v-mode)
-(package! realgud)
-(package! realgud-lldb)
-(package! devdocs)
+(package! v-mode
+  :recipe (:host github :repo "elogir/v-mode"))
+
+;; (package! v-mode)
+;; (package! realgud)
+;; (package! realgud-lldb)
+;; (package! devdocs)
 ;; (package! treesit-auto)
-(package! d-mode)
+;; (package! d-mode)
 
-(package! org :recipe
-  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
-   (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
-   :files
-   (:defaults "etc")
-   :build t :pre-build
-   (with-temp-file "org-version.el"
-     (require 'lisp-mnt)
-     (let
-         ((version
-           (with-temp-buffer
-             (insert-file-contents "lisp/org.el")
-             (lm-header "version")))
-          (git-version
-           (string-trim
-            (with-temp-buffer
-              (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
-              (buffer-string)))))
-       (insert
-        (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
-        (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
-        "(provide 'org-version)\n"))))
-  :pin nil)
+;; (package! org :recipe
+;;   (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+;;    (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+;;    :files
+;;    (:defaults "etc")
+;;    :build t :pre-build
+;;    (with-temp-file "org-version.el"
+;;      (require 'lisp-mnt)
+;;      (let
+;;          ((version
+;;            (with-temp-buffer
+;;              (insert-file-contents "lisp/org.el")
+;;              (lm-header "version")))
+;;           (git-version
+;;            (string-trim
+;;             (with-temp-buffer
+;;               (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+;;               (buffer-string)))))
+;;        (insert
+;;         (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+;;         (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+;;         "(provide 'org-version)\n"))))
+;;   :pin nil)
 
-(unpin! org)
+;; (unpin! org)
 
-(package! auctex)
-(package! aas)
-(package! toc-org)
-(package! engrave-faces)
+;; (package! auctex)
+;; (package! aas)
+;; (package! toc-org)
+;; (package! engrave-faces)
+;; (package! nov)
